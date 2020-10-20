b27b04bcb375d98265587d906a1dedb40d406f4f
opsbinxu <47974151+opsbinxu@users.noreply.github.com>
2020-10-20 22:42:51 +1100

    board set up

--- Knight's Tour/Setting up the board/knightstour/game.py
+++ Knight's Tour/Setting up the board/knightstour/game.py
@@ -1,3 +1,26 @@
-x = input('Enter your name: ')
-print('Hello, ' + x)
-print('Nice to meet you!')
+def print_board(x, y):
+    border = " -------------------"
+    xlabel = "  1 2 3 4 5 6 7 8 "
+    nrows = 8
+    ncols = 8
+
+    print(border)
+    for row in range(nrows):
+        print(nrows - row,  "|", end=' ', sep='')
+        for col in range(ncols):
+            if x == (col + 1) and y == (nrows - row):
+                print("X", end=' ')
+            else:
+                print("_", end=' ')
+        print("|")
+    print(border)
+    print(xlabel)
+
+
+def main():
+    x, y = map(int, input("Enter knight's starting position: ").split())
+    print_board(x, y)
+
+
+if __name__ == "__main__":
+    main()
--- Knight's Tour/That's not a chessboard!/knightstour/game.py
+++ Knight's Tour/That's not a chessboard!/knightstour/game.py
@@ -1,3 +1,60 @@
-x = input('Enter your name: ')
-print('Hello, ' + x)
-print('Nice to meet you!')
+def digits(num):
+    return len(str(num))
+
+
+def print_board(x, y, ncols, nrows):
+
+    # border = " " * yaxiswidth + "---"
+
+    yaxiswidth = digits(nrows)
+    # xaxiswidth = digits(nrows * ncols)
+    xaxiswidth = 1
+
+    xlabel = [" " * (yaxiswidth + 1)]
+    for col in range(ncols):
+        colnum = col + 1
+        xlabel.append(" " * (xaxiswidth - digits(colnum)) + str(colnum))
+
+    border = " " * yaxiswidth + "-" + "-" * (xaxiswidth + 1) * ncols + "--"
+    print(border)
+    for row in range(nrows):
+        rownum = nrows - row
+        print(" " * (yaxiswidth - digits(rownum)),
+              rownum,  "|", end=' ', sep='')
+        for col in range(ncols):
+            colnum = col + 1
+            if x == colnum and y == rownum:
+                print(" " * (xaxiswidth - 1), "X", end=' ', sep='')
+            else:
+                print("_" * xaxiswidth, end=' ')
+        print("|")
+    print(border)
+
+    print(*xlabel)
+
+
+def main():
+    while True:
+        try:
+            ncols, nrows = map(int, input(
+                "Enter your board's dimensions: ").split())
+        except ValueError:
+            print("Invalid entry!")
+            continue
+        else:
+            break
+    while True:
+        try:
+            x, y = map(int, input("Enter knight's starting position: ").split())
+            if x > ncols or y > nrows:
+                raise ValueError('Invalid position!')
+        except ValueError:
+            print("Invalid position!")
+            continue
+        else:
+            break
+    print_board(x, y, ncols, nrows)
+
+
+if __name__ == "__main__":
+    main()
