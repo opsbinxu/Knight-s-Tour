6b417d2ebda1a1390c78b4fe21e0b829b0e8d2b4
opsbinxu <47974151+opsbinxu@users.noreply.github.com>
2020-10-21 00:23:34 +1100

    stage 3 code

--- Knight's Tour/Looking ahead/knightstour/game.py
+++ Knight's Tour/Looking ahead/knightstour/game.py
@@ -1,3 +1,101 @@
-x = input('Enter your name: ')
-print('Hello, ' + x)
-print('Nice to meet you!')
+DIRECTIONS = 8
+
+
+def digits(num):
+    return len(str(num))
+
+
+def printBoard(x, y, ncols, nrows, board):
+    board = board[::-1]
+
+    # determine x and y axis number width
+    yaxiswidth = digits(nrows)
+    xaxiswidth = digits(nrows * ncols)
+    # xaxiswidth = 1
+
+    # generate x axis labels
+    xlabel = [" " * (yaxiswidth + 1)]
+    for col in range(ncols):
+        colnum = col + 1
+        # xlabel.append(" " * (xaxiswidth - digits(colnum)) + str(colnum))
+        # xlabel.append("{num:{fill}{width}}".format(num=colnum, fill=" ", width=xaxiswidth))
+        xlabel.append(str(colnum).rjust(xaxiswidth))
+
+    # generate top and bottom border
+    border = " " * yaxiswidth + "-" + "-" * (xaxiswidth + 1) * ncols + "--"    
+
+    # print out board
+    print(border)
+
+    for y, row in enumerate(board):
+        rownum = nrows - y
+        print(str(rownum).rjust(yaxiswidth), "|", end=' ', sep='')
+        for x, col in enumerate(row):
+            if col == -1:
+                print("_" * xaxiswidth, end=' ')
+            else:
+                print(col.rjust(xaxiswidth), end=' ', sep='')
+        print("|")
+    print(border)
+
+    # print out x labels
+    print(*xlabel)
+
+
+def validMove(x, y, ncols, nrows, board):  # user coordinates 1 - n
+    if not onBoard(x, y, ncols, nrows):
+        return False
+
+    if not board[y-1][x-1] == -1:
+        return False
+    return True
+
+
+def onBoard(x, y, ncols, nrows):  # user coordinates 1 - n
+    if x > 0 and y > 0 and x <= ncols and y <= nrows:
+        return True
+    return False
+
+
+def checkMove(cur_x, cur_y, ncols, nrows, board):
+    move_x = [2, 1, -1, -2, -2, -1, 1, 2]
+    move_y = [1, 2, 2, 1, -1, -2, -2, -1]
+    for i in range(DIRECTIONS):
+        new_x = cur_x + move_x[i]   # user coordinates 1 - n
+        new_y = cur_y + move_y[i]   # user coordinates 1 - n
+        if validMove(new_x, new_y, ncols, nrows, board):
+            board[new_y-1][new_x-1] = "O"
+
+
+def main():
+    while True:
+        try:
+            ncols, nrows = map(int, input(
+                "Enter your board's dimensions: ").split())
+        except ValueError:
+            print("Invalid entry!")
+            continue
+        else:
+            board = [[-1 for i in range(ncols)] for i in range(nrows)]
+            break
+    while True:
+        try:
+            x, y = map(int, input("Enter knight's starting position: ").split())
+            if not onBoard(x, y, ncols, nrows):
+                raise ValueError
+        except ValueError:
+            print('Invalid position!')
+            continue
+        else:
+            board[y-1][x-1] = "X"
+            break
+
+    checkMove(x, y, ncols, nrows, board)
+    # print(*board[::-1], sep="\n")
+
+    print("\nHere are the possible moves:")
+    printBoard(x, y, ncols, nrows, board)
+
+
+if __name__ == "__main__":
+    main()
--- Knight's Tour/That's not a chessboard!/knightstour/game.py
+++ Knight's Tour/That's not a chessboard!/knightstour/game.py
@@ -1,22 +1,24 @@
 def digits(num):
     return len(str(num))
 
-
 def print_board(x, y, ncols, nrows):
-
-    # border = " " * yaxiswidth + "---"
-
+    # determine x and y axis number width
     yaxiswidth = digits(nrows)
-    # xaxiswidth = digits(nrows * ncols)
-    xaxiswidth = 1
+    xaxiswidth = digits(nrows * ncols)
+    # xaxiswidth = 1
 
+    # generate x axis labels
     xlabel = [" " * (yaxiswidth + 1)]
     for col in range(ncols):
         colnum = col + 1
         xlabel.append(" " * (xaxiswidth - digits(colnum)) + str(colnum))
 
+    # generate top and bottom border
     border = " " * yaxiswidth + "-" + "-" * (xaxiswidth + 1) * ncols + "--"
+
     print(border)
+
+    # print out board
     for row in range(nrows):
         rownum = nrows - row
         print(" " * (yaxiswidth - digits(rownum)),
@@ -29,9 +31,14 @@ def print_board(x, y, ncols, nrows):
                 print("_" * xaxiswidth, end=' ')
         print("|")
     print(border)
-
+    
+    # print out x labels
     print(*xlabel)
 
+def invalidMove(x, y, ncols, nrows):
+    if x < 1 or y < 1 or x > ncols or y > nrows:
+        return True
+    return False
 
 def main():
     while True:
@@ -46,10 +53,10 @@ def main():
     while True:
         try:
             x, y = map(int, input("Enter knight's starting position: ").split())
-            if x > ncols or y > nrows:
-                raise ValueError('Invalid position!')
+            if invalidMove(x, y, ncols, nrows):
+                raise ValueError
         except ValueError:
-            print("Invalid position!")
+            print('Invalid position!')
             continue
         else:
             break
