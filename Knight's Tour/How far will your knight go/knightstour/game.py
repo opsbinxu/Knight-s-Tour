4d50ee852b78126e9d851080a1983782e0d0ba51
opsbinxu <47974151+opsbinxu@users.noreply.github.com>
2020-10-21 22:01:42 +1100

    stage 5 completed

--- Knight's Tour/How far will your knight go/knightstour/game.py
+++ Knight's Tour/How far will your knight go/knightstour/game.py
@@ -47,13 +47,27 @@ def printBoard(ncols, nrows, board):
     print(*xlabel)
 
 
-def validMove(x, y, ncols, nrows, board):  # user coordinates 1 - n
+def validMove(x, y, past_x, past_y, ncols, nrows, board):  # user coordinates 1 - n
+    if not freeSpot(x, y, ncols, nrows, board):
+        return False
+    if not knightsMove(x, y, past_x, past_y):
+        return False
+    return True
+
+
+def freeSpot(x, y, ncols, nrows, board):
     if not onBoard(x, y, ncols, nrows):
         return False
 
     if not board[y-1][x-1] == -1:
         return False
     return True
+
+
+def knightsMove(x, y, last_x, last_y):
+    if (abs(x-last_x) == 1 and abs(y-last_y) == 2) or (abs(x-last_x) == 2 and abs(y-last_y) == 1):
+        return True
+    return False
 
 
 def onBoard(x, y, ncols, nrows):  # user coordinates 1 - n
@@ -63,29 +77,64 @@ def onBoard(x, y, ncols, nrows):  # user coordinates 1 - n
 
 
 def checkMove(cur_x, cur_y, ncols, nrows, board):
-    # move_x = [2, 1, -1, -2, -2, -1, 1, 2]
-    # move_y = [1, 2, 2, 1, -1, -2, -2, -1]
     global deadend
     deadend = True
     for i in range(DIRECTIONS):
         new_x = cur_x + move_x[i]   # user coordinates 1 - n
         new_y = cur_y + move_y[i]   # user coordinates 1 - n
-        if validMove(new_x, new_y, ncols, nrows, board):
+        if freeSpot(new_x, new_y, ncols, nrows, board):
             board[new_y-1][new_x -
                            1] = warnsdorff(new_x, new_y, ncols, nrows, board)
             deadend = False
 
 
 def warnsdorff(cur_x, cur_y, ncols, nrows, board):
-    # move_x = [2, 1, -1, -2, -2, -1, 1, 2]
-    # move_y = [1, 2, 2, 1, -1, -2, -2, -1]
     possible = 0
     for i in range(DIRECTIONS):
         new_x = cur_x + move_x[i]   # user coordinates 1 - n
         new_y = cur_y + move_y[i]   # user coordinates 1 - n
-        if validMove(new_x, new_y, ncols, nrows, board):    # user coordinates 1 - n
+        if freeSpot(new_x, new_y, ncols, nrows, board):    # user coordinates 1 - n
             possible += 1
     return possible
+
+
+def play_game(start_x, start_y, ncols, nrows, board):
+    global win, deadend
+    last_x, last_y = start_x, start_y
+    warns_board = deepcopy(board)
+    checkMove(start_x, start_y, ncols, nrows, warns_board)
+    printBoard(ncols, nrows, warns_board), print("")
+
+    moves = 1
+    spaces = ncols * nrows
+
+    while not win and not deadend:
+        while True:
+            try:
+                x, y = map(int, input("Enter your next move: ").split())
+                if not onBoard(x, y, ncols, nrows):
+                    raise ValueError
+                if not validMove(x, y, last_x, last_y, ncols, nrows, board):
+                    raise ValueError
+            except ValueError:
+                print('Invalid move! ', end='')
+                continue
+            else:
+                board[y-1][x-1] = "X"
+                board[last_y-1][last_x-1] = "*"
+                last_x, last_y = x, y
+                warns_board = deepcopy(board)
+                checkMove(x, y, ncols, nrows, warns_board)
+                printBoard(ncols, nrows, warns_board), print("")
+                moves += 1
+                break
+    if moves == spaces:
+        win = True
+        print("Your knight toured the whole board!")
+    elif deadend:
+        print("No more possible moves!")
+        print(f"Your knight visted {moves} space",
+              "s" * (1 if (moves > 1) else 0), "!", sep="")
 
 
 def main():
@@ -112,79 +161,7 @@ def main():
             break
 
     play_game(x, y, ncols, nrows, board)
-    # warns_board = deepcopy(board)
-    # checkMove(x, y, ncols, nrows, warns_board)
-    # printBoard(ncols, nrows, warns_board), print("")
 
-    # moves = 1
-    # spaces = ncols * nrows
-
-    # while not win and not deadend:
-    #     while True:
-    #         try:
-    #             x, y = map(int, input("Enter your next move: ").split())
-    #             if not onBoard(x, y, ncols, nrows):
-    #                 raise ValueError
-    #             if not validMove(x, y, ncols, nrows, board):
-    #                 raise ValueError
-    #         except ValueError:
-    #             print('Invalid move! ', end='')
-    #             continue
-    #         else:
-    #             board[y-1][x-1] = "X"
-    #             board[last_y-1][last_x-1] = "*"
-    #             last_x, last_y = x, y
-    #             warns_board = deepcopy(board)
-    #             checkMove(x, y, ncols, nrows, warns_board)
-    #             printBoard(ncols, nrows, warns_board), print("")
-    #             moves += 1
-    #             if moves == spaces:
-    #                 win = True
-    #                 print("Your knight toured the whole board!")
-    #             if deadend:
-    #                 print("No more possible moves!")
-    #                 print(f"Your knight moved {moves} space",
-    #                     "s" * (1 if (moves > 1) else 0), "!", sep="")
-    #             break
-
-
-def play_game(start_x, start_y, ncols, nrows, board):
-    global win, deadend
-    last_x, last_y = start_x, start_y
-    warns_board = deepcopy(board)
-    checkMove(start_x, start_y, ncols, nrows, warns_board)
-    printBoard(ncols, nrows, warns_board), print("")
-
-    moves = 1
-    spaces = ncols * nrows
-
-    while not win and not deadend:
-        while True:
-            try:
-                x, y = map(int, input("Enter your next move: ").split())
-                if not onBoard(x, y, ncols, nrows):
-                    raise ValueError
-                if not validMove(x, y, ncols, nrows, board):
-                    raise ValueError
-            except ValueError:
-                print('Invalid move! ', end='')
-                continue
-            else:
-                board[y-1][x-1] = "X"
-                board[last_y-1][last_x-1] = "*"
-                last_x, last_y = x, y
-                warns_board = deepcopy(board)
-                checkMove(x, y, ncols, nrows, warns_board)
-                printBoard(ncols, nrows, warns_board), print("")
-                moves += 1
-                if moves == spaces:
-                    win = True
-                    print("Your knight toured the whole board!")
-                elif deadend:
-                    print("No more possible moves!")
-                    print(f"Your knight moved {moves} space",
-                        "s" * (1 if (moves > 1) else 0), "!", sep="")
-                break
 
 if __name__ == "__main__":
     main()
